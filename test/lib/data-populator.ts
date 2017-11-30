767307b3edb184bd23a57a7f8a19fef240b54cbf
kaveh.fereidouni <kaveh981@gmail.com>
2017-11-29 22:13:24 -0500

    [MGM-43] add more method to data populator

--- test/lib/data-populator.ts
+++ test/lib/data-populator.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import { GenericRepository } from '../../data-layer';
-import { Employees, Users } from '../../model-layer';
+import { Employees, Users, Roles, Menus } from '../../model-layer';
 
 
 /**
@@ -28,11 +28,66 @@ class DataPopulator {
 
     }
 
+    public async createRole(role?: Roles): Promise<Roles> {
+        role = role || new Roles();
+        role.role = role.role || 'someRole';
+        return await this.repo.save(role);
+    }
+
+    public async createMenu(isMain: boolean, menu?: Menus): Promise<Menus> {
+        menu = menu || new Menus();
+        menu.title = menu.title || 'someTitle';
+        if (!isMain) {
+            let parent = menu.parent || new Menus();
+            parent.title = 'parentMenu';
+            menu.parent = parent;
+            let result = await this.repo.save(menu);
+            console.log('added menu: ' + JSON.stringify(result));
+            return result;
+        }
+        return await this.repo.save(menu);
+    }
+
     public async getUserById(id: number): Promise<Users> {
         let result: Users = await this.repo.getSingle(Users, {
             relations: ["employee"],
             where: {
                 "userId": id
+            }
+        });
+        return result
+    }
+
+    public async getRoleById(id: number): Promise<Roles> {
+        let result: Roles = await this.repo.getSingle(Roles, {
+            where: {
+                "roleId": id
+            }
+        });
+        return result
+    }
+    public async getMenuById(id: number): Promise<Menus> {
+        let result: Menus = await this.repo.getSingle(Menus, {
+            relation: ['subMenus'],
+            where: {
+                menuId: id
+            }
+        });
+        return result
+    }
+    public async  getMainMenus(): Promise<Menus[]> {
+        let result: Menus[] = await this.repo.list(Menus, {
+            where: {
+                parentMenuId: 'IS NULL'
+            }
+        });
+        return result;
+    }
+    public async getSubMenusByParentId(id: number): Promise<Menus[]> {
+        let result: Menus[] = await this.repo.list(Menus, {
+            relation: ['subMenus'],
+            where: {
+                parentMenuId: id
             }
         });
         return result
